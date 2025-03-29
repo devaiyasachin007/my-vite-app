80c00ee4ee6bdd2843d65627b6235d3b02e78657
Sachin Devaiya <devaiyasachin@gmail.com>
2025-03-29 20:17:20 +0530

    feat: remove seo code

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import '@/assets/main.scss';
 import { createPinia } from "pinia";
 import router from "@/router/movies.router";
-import VueMeta from 'vue-meta';
 
 const pinia = createPinia();
 const app = createApp(App).use(pinia).use(router).mount("#app");
-app.use(VueMeta);
