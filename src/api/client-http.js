ec3ac57e62ec83751492c00a8dee212b4a2291be
Sachin Devaiya <devaiyasachin@gmail.com>
2025-03-29 16:22:58 +0530

    feat: update api for cross origin

--- src/api/client-http.js
+++ src/api/client-http.js
@@ -4,7 +4,7 @@ class ClientHTTP {
   constructor() {
     this.apiKey = "9cf6387a";
     this.api = axios.create({
-      baseURL: "http://www.omdbapi.com",
+      baseURL: "https://www.omdbapi.com",
       headers: {
         "Content-Type": "application/json",
       },
