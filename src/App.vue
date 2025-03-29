f7094269d9653ec18387509a1758343a2ac9754b
Sachin Devaiya <devaiyasachin@gmail.com>
2025-03-29 16:15:32 +0530

    feat: update config

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
 </template>
 
 <script setup>
-import Header from "@/components/header/Header.vue";
-import MoviesList from "@/components/movies/MoviesList.vue";
+// import Header from "@/components/header/Header.vue";
+// import MoviesList from "@/components/movies/MoviesList.vue";
 import Footer from "@/components/footer/Footer.vue";
 </script>
  
